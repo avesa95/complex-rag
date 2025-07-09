b4c408ed8044960a55304a0e0c273bb6fae41789
Vesa Alexandru <vesaalexandru95@gmail.com>
2025-07-09 17:45:05 +0300

    added react app

---
+++ pdf-upload-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PDFUploadApp from './components/PDFUploadApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PDFUploadApp />
+    </div>
+  );
+}
+
+export default App;
